91e81fc7ff171a1f28e64a1f2ca7df3de0609e93
AlexinaO <alexina.orsoni@gmail.com>
2018-09-13 17:07:58 +0200

    Ajout du constructeur de la classe connexion qu'il faut mettre quand le nom de la chaîne de connexion est différent de la classe

--- TodoList/Data/TodoDbContext.cs
+++ TodoList/Data/TodoDbContext.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using TodoList.Models;
 
 namespace TodoList.Data
 {
-    public class TodoDbContext:DbContext
+    public class TodoDbContext : DbContext
 
     {
+        public TodoDbContext() : base("TodoListConnectionString")
+        {
+        }
+
+        public DbSet<Categorie> Categories {get;set;}
     }
 }
