f1d445368a4bf63c2111b16246e2213d68eae700
AlexinaO <alexina.orsoni@gmail.com>
2018-09-13 16:47:32 +0200

    Création de la classe TodoDbContext avec using System.Data.Entity

--- TodoList/Data/TodoDbContext.cs
+++ TodoList/Data/TodoDbContext.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace TodoList.Data
 {
-    public class TodoDbContext
+    public class TodoDbContext:DbContext
+
     {
     }
 }
