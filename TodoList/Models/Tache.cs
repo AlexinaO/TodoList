87fb659ef36f1bfb11f0d8773a00945828776a44
AlexinaO <alexina.orsoni@gmail.com>
2018-09-13 16:36:51 +0200

    Ajout des paramètres de la classe Tache

--- TodoList/Models/Tache.cs
+++ TodoList/Models/Tache.cs
@@ -7,5 +7,12 @@ namespace TodoList.Models
 {
     public class Tache
     {
+        public int ID { get; set; }
+        public string Nom { get; set; }
+        public string Description { get; set; }
+        public DateTime DateFin { get; set; }
+        public bool Statut { get; set; }
+        public int Priorite { get; set; }
+
     }
 }
