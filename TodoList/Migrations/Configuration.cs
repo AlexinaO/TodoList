c0d9c1fdc509030318709717665d3aa2b6b6c187
AlexinaO <alexina.orsoni@gmail.com>
2018-09-14 10:36:52 +0200

    Création de la classe Configuration

---
+++ TodoList/Migrations/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using TodoList.Data;
+
+namespace TodoList.Migrations
+{
+    public class Configuration:DbMigrationsConfiguration<TodoDbContext>
+    {
+    }
+}
